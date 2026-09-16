584296a559e2681ea02f3825ea9a5326285ff156
honeydog3115 <tjs91002256@gmail.com>
2026-09-16 16:52:09 +0900

    상속 연습 2

--- name.py
+++ name.py
@@ -1,38 +1,19 @@
-class FirstName:
-    def __init__(self, first_name):
-        self.first_name = first_name
-
-class MySecondName(FirstName):
-    def __init__(self, second_name):
-        super().__init__()
-        self.second_name = second_name
-        
-    def __str__(self):
-        return self.first_name + self.second_name
-        
-class MySisterName(FirstName):
-    def __init__(self):
-        super().__init__()
-        self.second_name = "민지"
-        
-    def __str__(self):
-        return self.first_name + self.second_name
-
-first = FirstName("선")
-me = MySecondName("종범")
-sister = MySisterName()
-print(me)
-print(sister)
-
 class Family_name:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, family_name, main_building="", root=""):
+        self.family_name = family_name
+        self.main_building = main_building
+        self.root = root
     
-class Last_name(Family_name):
-    def __init__(self, name):
-        self.name = name
+class Full_name(Family_name):
+    def __init__(self, family_name, last_name, main_building, root):
+        super().__init__(family_name, main_building, root)
+        self.last_name = last_name
+    
+    def name_print(self):
+        print(self.family_name + self.last_name)
         
-a = Family_name("홍")
-b = Last_name("길동")
-full_name = a.name + b.name
-print(full_name)
+test = Full_name("홍", "길동", "", "")
+print(test.family_name)
+print(test.main_building)
+print(test.root)
+test.name_print()
